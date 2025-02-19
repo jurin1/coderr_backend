b28c04c3168fc8351de531a16b90567cc018ec3a
Juri <neizcon@gmail.com>
2025-02-19 19:30:22 +0100

    Initial commit: Set up Django project structure with basic configurations and admin integration for Token model.

---
+++ coderr_app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from rest_framework.authtoken.models import Token
+
+@admin.register(Token)
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ('key', 'user', 'created')
