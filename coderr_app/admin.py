0daa6c2a8d558daa397c2fcadb9218b4d5a32b7d
Juri <neizcon@gmail.com>
2025-02-24 17:43:28 +0100

    Füge ProfileAdmin zur Admin-Oberfläche hinzu, um Benutzerprofile besser zu verwalten und anzuzeigen.

--- coderr_app/admin.py
+++ coderr_app/admin.py
@@ -4,7 +4,19 @@ from rest_framework.authtoken.models import Token
 from .models import CustomUser, Profile, FileUpload 
 from django.utils.html import format_html
 
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name', 'last_name', 'email', 'created_at')  
+    search_fields = ('user__username', 'first_name', 'last_name', 'email')  
+    list_filter = ('created_at',) 
+    raw_id_fields = ('user',)
 
+    def email(self, obj):
+        return obj.user.email
+    email.short_description = 'User Email' 
+
+  
+    readonly_fields = ('created_at',)
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
