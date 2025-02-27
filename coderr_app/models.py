ab06c93c46756b6bac2a40d1f664a48060e20a4b
Juri <neizcon@gmail.com>
2025-02-27 17:10:33 +0100

    fix working hours char length to 500

--- coderr_app/models.py
+++ coderr_app/models.py
@@ -22,7 +22,7 @@ class Profile(models.Model):
     location = models.CharField(max_length=100, blank=True)
     tel = models.CharField(max_length=20, blank=True)
     description = models.TextField(blank=True)
-    working_hours = models.CharField(max_length=50, blank=True)
+    working_hours = models.CharField(max_length=500, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
